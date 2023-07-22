34ef34f3219ac6fdaa8f5952ecb2082149ef8f50
Gelo Ventenilla <angelocarlo.ventenilla@benilde.edu.ph>
2023-07-23 01:03:28 +0800

    added temporary Play Again function

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] GameObject player;
     [SerializeField] GameObject textObj;
+    [SerializeField] GameObject playAgainButton;
     public void GameOver()
     {
         player.gameObject.SetActive(false);
         textObj.gameObject.SetActive(true);
+        playAgainButton.gameObject.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("SampleScene");
+        playAgainButton.gameObject.SetActive(false);
     }
 }
