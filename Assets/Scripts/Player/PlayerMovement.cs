5ba284131539b4852ee25a4384ffd3e935393f9f
cliffy <ignisalt3@gmail.com>
2023-07-23 10:19:37 +0800

    Pushed new input system (WIP)
    - input system works but the player is jiterring ( looking for a fix now)

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,41 +1,53 @@
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public bool isMovingSideward { get; private set; } = false;
     public float distanceTravelled { get; private set; } = 0;
-    private Vector3 Direction;
+    public Vector3 Direction;
     public Rigidbody rigidBody;
-    [SerializeField] private float sideMovementSpeed;
+    public float sideMovementSpeed;
     [SerializeField] private float swipeSensitivity;
     [SerializeField] private float forwardSpeed;
-
+    private int xAxis = 0;
     void Update()
     {
 
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            isMovingSideward = true;
-        }
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    isMovingSideward = true;
+        //}
 
-        if (Input.GetMouseButtonUp(0))
-        {
-            isMovingSideward = false;
-        }
+        //if (Input.GetMouseButtonUp(0))
+        //{
+        //    isMovingSideward = false;
+        //}
 
 
         if (isMovingSideward)
         {
-            Direction.x = Mathf.Lerp(Direction.x, Input.GetAxis("Mouse X"), Time.deltaTime * sideMovementSpeed);
+            Debug.Log("Mouse X:" + Input.GetAxis("Mouse X"));
+            //Direction.x = UnityEngine.InputSystem.EnhancedTouch.Touch.fingers[0].screenPosition.x * sideMovementSpeed*Time.deltaTime;
+            //   Direction.x = Mathf.Lerp(Direction.x, UnityEngine.InputSystem.EnhancedTouch.Touch.fingers[0].screenPosition.x, Time.deltaTime * sideMovementSpeed);
 
             Direction = Vector3.ClampMagnitude(Direction, 1f);
         }
             this.transform.position += new Vector3(0f, 0f,  Time.deltaTime * forwardSpeed);
         
     }
-    
+
+    public void MovePlayer()
+    {
+        isMovingSideward = true;
+    }
+
+    public void StopPlayer()
+    {
+        isMovingSideward = false;
+    }
+
     private void FixedUpdate()
     {
       
