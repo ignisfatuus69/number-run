e02868c49ec9d80513e6db3d9b3eebb940aa0c2f
cliffy <ignisalt3@gmail.com>
2023-08-07 20:41:21 +0800

    New shield power up implementation (WIP)
    -New shield power up code has been implemented
    - WIP (the shield power up still needs visual feedback)

--- Assets/Scripts/Powerups/ShieldPowerUp.cs
+++ Assets/Scripts/Powerups/ShieldPowerUp.cs
@@ -15,7 +15,7 @@ public class ShieldPowerUp : PowerUp
     //Have these values set by the spawners
 
     [HideInInspector]public GameObject shieldObj;
-    [HideInInspector]public EquationChecker equationCheckerObj;
+    public EquationChecker equationCheckerObj;
     float currentDuration;
 
     private void OnEnable()
@@ -31,6 +31,7 @@ public class ShieldPowerUp : PowerUp
         base.Effect(other);
         shieldObj.gameObject.SetActive(true);
         equationCheckerObj.isImmuneToEquation = true;
+        StartCoroutine(DestroyObstacleWithLazer());
         StartCoroutine(CountDownToDuration());
     }
 
@@ -42,13 +43,13 @@ public class ShieldPowerUp : PowerUp
         AudioManager.Instance.PlayOneShot(powerUpSFX);
     }
 
+   // IEnumerator
     IEnumerator CountDownToDuration()
     {
         while (currentDuration > 0)
         {
             yield return new WaitForSeconds(0.1f);
             currentDuration -= 0.1f;
-
             if (currentDuration <= 0)
             {
                 Debug.Log("Shield has worn off");
@@ -56,6 +57,28 @@ public class ShieldPowerUp : PowerUp
                 OnShieldEnded?.Invoke(this.gameObject);
                 shieldObj.gameObject.SetActive(false);
                 isActive = false;
+                StopAllCoroutines();
+            }
+        }
+    }
+
+    IEnumerator DestroyObstacleWithLazer()
+    {
+        while (currentDuration > 0)
+        {
+            yield return new WaitForSeconds(0.1f);
+            if (!equationCheckerObj.isImmuneToEquation) yield return null;
+            if (equationCheckerObj?.correctObstacle != null)
+            {
+                if (equationCheckerObj.correctObstacle.isActiveAndEnabled)
+                {
+                    Vector2 TargetDirection = ((equationCheckerObj.correctObstacle.transform.position) -(equationCheckerObj.transform.position)).normalized;
+                    Debug.DrawLine(equationCheckerObj.transform.position, equationCheckerObj.transform.TransformDirection(new Vector3(TargetDirection.x,TargetDirection.y,-1f)) , Color.red, 5f);
+                    Debug.Log(equationCheckerObj.transform.TransformDirection(new Vector3(TargetDirection.x, TargetDirection.y, 1f)));
+                    yield return new WaitForSeconds(1f);
+                    equationCheckerObj.correctObstacle.PlayerInteraction(equationCheckerObj);
+                    Debug.Log("Destroy obstacle");
+                }
             }
         }
     }
