44071d13cbd38334083070afcf971efbed8c56f7
GeloVentenilla <gelo.ventenilla@gmail.com>
2023-10-17 16:16:48 +0800

    implemented coins sfx

--- Assets/Scripts/Coins/CoinInventory.cs
+++ Assets/Scripts/Coins/CoinInventory.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class CoinInventory : MonoBehaviour
 {
     [SerializeField] int coinMultiplier= 1;
+    [SerializeField] AudioClip coinSFX;
     public int coinAmount { get; private set; }
     public void AddCoin(int additive)
     {
         coinAmount += additive * coinMultiplier;
+        AudioManager.Instance.PlayOneShot(coinSFX);
     }
 
     public void ReduceCoin(int subtrahend)
